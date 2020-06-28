2a0b41b4f4f22193cc43ff808245462414333598
keddisa <keddisa@gmail.com>
2020-06-28 09:04:45 -0400

    add logic for a failed attempt

--- script.js
+++ script.js
@@ -32,34 +32,52 @@
     }
 
     $(".color-button").click(function(){
-        let colorClicked = $(this).attr("data-value")
-        new Audio("sounds/" + colorClicked + ".mp3").play()
-        $("#"+colorClicked).animate({  textIndent: 0 }, {
-            step: function() {
-              $(this).css('-webkit-transform','translateY(-.2rem)')
-              $(this).css('filter','brightness(50%)')
-            },
-            duration:'fast'
-        },'swing').animate({  textIndent: 0 }, {
-            step: function() {
-              $(this).css('-webkit-transform','translateY(.2rem)')
-              $(this).css('filter','brightness(100%)')
-            },
-            duration:'fast'
-        },'swing')
-
-        if(patternToGuess[guessedPattern.length] === colorClicked) {
-            guessedPattern.push(colorClicked)
-            console.log(guessedPattern)
-            if(guessedPattern.length === patternToGuess.length) {
-                guessedPattern = []
-                setTimeout(() => {
-                    getNextPatternColor()
-                }, 1500);
-            }        
-        } else {
-            gameOver = true
-        }
+        if(!gameOver) {
+            let colorClicked = $(this).attr("data-value")
+            new Audio("sounds/" + colorClicked + ".mp3").play()
+            $("#"+colorClicked).animate({  textIndent: 0 }, {
+                step: function() {
+                  $(this).css('-webkit-transform','translateY(-.2rem)')
+                  $(this).css('filter','brightness(50%)')
+                },
+                duration:'fast'
+            },'swing').animate({  textIndent: 0 }, {
+                step: function() {
+                  $(this).css('-webkit-transform','translateY(.2rem)')
+                  $(this).css('filter','brightness(100%)')
+                },
+                duration:'fast'
+            },'swing')
+    
+            if(patternToGuess[guessedPattern.length] === colorClicked) {
+                guessedPattern.push(colorClicked)
+                console.log(guessedPattern)
+                if(guessedPattern.length === patternToGuess.length) {
+                    guessedPattern = []
+                    $("#message").text(successMessages[Math.floor(Math.random()*successMessages.length)])
+                    setTimeout(() => {
+                        getNextPatternColor()
+                    }, 1500);
+                }        
+            } else {
+                gameOver = true
+                new Audio("sounds/wrong.mp3").play()
+                $("body").animate({  textIndent: 0 }, {
+                    step: function() {
+                      $(this).removeClass("bg-indigo-100")
+                      $(this).addClass("bg-red-500")
+                    },
+                    duration:'fast'
+                },'swing').animate({  textIndent: 0 }, {
+                    step: function() {
+                      $(this).removeClass("bg-red-500")
+                      $(this).addClass("bg-indigo-100")
+                    },
+                    duration:'fast'
+                },'swing')
+                $("#message").text(failMessages[Math.floor(Math.random()*failMessages.length)])
+            }
+        }      
     })
 
     $("h1").on("click", startGame)
