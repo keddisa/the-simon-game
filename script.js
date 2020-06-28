17ad8ed2b3a66c3b9f5bd4a8e6da4de4983f1cca
keddisa <keddisa@gmail.com>
2020-06-28 08:31:01 -0400

    add event listener to buttons

--- script.js
+++ script.js
@@ -6,13 +6,13 @@
 
     const values = ["green", "blue", "yellow", "red"]
     const successMessages = ["Well done!", "Awesome job!", "You're killing it!", "Nice! Keep it up", "Nice!", "Great work. Next One!"]
-    const failMessages = ["Oops! Next time", "Auch. That went wrong.", "Good run. Better luck next time :)", "Ah well.. Good effort though"]
+    const failMessages = ["Oops! Next time..", "Auch. That went wrong.", "Good run. Better luck next time :)", "Ah well.. Good effort though"]
 
     // while(level<4) {
         
         level++
     // }
-    
+
     function nextColorInPattern() {
         patternToGuess.push(values[Math.floor(Math.random()*values.length)])
         console.log(patternToGuess)
@@ -20,6 +20,24 @@
         new Audio("sounds/" + patternToGuess[patternToGuess.length-1] + ".mp3").play()
     }
 
-    $(document).on("click", nextColorInPattern)
+    $(".color-button").click(function(){
+        let colorClicked = $(this).attr("data-value")
+        new Audio("sounds/" + colorClicked + ".mp3").play()
+        $("#"+colorClicked).animate({  textIndent: 0 }, {
+            step: function() {
+              $(this).css('-webkit-transform','translateY(-.2rem)')
+              $(this).css('filter','brightness(50%)')
+            },
+            duration:'1'
+        },'swing').animate({  textIndent: 0 }, {
+            step: function() {
+              $(this).css('-webkit-transform','translateY(.2rem)')
+              $(this).css('filter','brightness(100%)')
+            },
+            duration:'1'
+        },'swing')
+    })
+
+    $("h1").on("click", nextColorInPattern)
 })()
 
