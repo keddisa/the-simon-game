860c8e9b6886fe83a8a6eabcc8af16f6ff20f1e1
keddisa <keddisa@gmail.com>
2020-06-27 22:47:40 -0400

    enclose in iife

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const patternToGuess = []
-let level = 1
-let highestScore = 0
-let gameOver = false
+(function() {
+    const patternToGuess = []
+    let level = 1
+    let highestScore = 0
+    let gameOver = false
 
-const values = [1, 2, 3, 4]
-const successMessages = ["Well done!", "Awesome job!", "You're killing it!", "Nice! Keep it up", "Nice!", "Great work. Next One!"]
-const failMessages = ["Oops! Next time", "Auch. That went wrong.", "Good run. Better luck next time :)", "Ah well.. Good effort though"]
+    const values = [1, 2, 3, 4]
+    const successMessages = ["Well done!", "Awesome job!", "You're killing it!", "Nice! Keep it up", "Nice!", "Great work. Next One!"]
+    const failMessages = ["Oops! Next time", "Auch. That went wrong.", "Good run. Better luck next time :)", "Ah well.. Good effort though"]
+
+})()
+
