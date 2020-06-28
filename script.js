ba96378320dfe0a9df8f8410c98c2c10bec9e0fa
keddisa <keddisa@gmail.com>
2020-06-28 07:59:35 -0400

    add animations and sounds to buttons

--- script.js
+++ script.js
@@ -4,9 +4,22 @@
     let highestScore = 0
     let gameOver = false
 
-    const values = [1, 2, 3, 4]
+    const values = ["green", "blue", "yellow", "red"]
     const successMessages = ["Well done!", "Awesome job!", "You're killing it!", "Nice! Keep it up", "Nice!", "Great work. Next One!"]
     const failMessages = ["Oops! Next time", "Auch. That went wrong.", "Good run. Better luck next time :)", "Ah well.. Good effort though"]
 
+    // while(level<4) {
+        
+        level++
+    // }
+    
+    function nextColorInPattern() {
+        patternToGuess.push(values[Math.floor(Math.random()*values.length)])
+        console.log(patternToGuess)
+        $("#" + patternToGuess[patternToGuess.length-1]).hide(50).show(50)
+        new Audio("sounds/" + patternToGuess[patternToGuess.length-1] + ".mp3").play()
+    }
+
+    $(document).on("click", nextColorInPattern)
 })()
 
