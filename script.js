a0d5fd74aa6334c25690f6fc99bc9a6e4831f7a9
keddisa <keddisa@gmail.com>
2020-06-27 22:45:32 -0400

    set up some initial variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const patternToGuess = []
+let level = 1
+let highestScore = 0
+let gameOver = false
+
+const values = [1, 2, 3, 4]
+const successMessages = ["Well done!", "Awesome job!", "You're killing it!", "Nice! Keep it up", "Nice!", "Great work. Next One!"]
+const failMessages = ["Oops! Next time", "Auch. That went wrong.", "Good run. Better luck next time :)", "Ah well.. Good effort though"]
